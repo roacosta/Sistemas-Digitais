3b9956d00833a7f0f9be58622733be08caf2ef5c
roacosta <digo_acosta@hotmail.com>
2016-03-03 22:30:18 -0300

    Update Piscaled.v

--- Piscaled.v
+++ Piscaled.v
@@ -1,2 +1,26 @@
 # Sistemas-Digitais
 Conteúdo referente a disciplina de Sistemas Digitais
+module Pisca(input op,
+             output led);
+
+    assign led = op;
+
+
+endmodule
+
+module test;
+    reg op;
+    wire led;
+
+    Pisca A(op,led);
+
+    always #2 op <= ~op;
+
+    initial begin
+        $dumpvars(0 , A);
+        op <= 0;
+        #50;
+        $finish;
+    end
+
+endmodule
