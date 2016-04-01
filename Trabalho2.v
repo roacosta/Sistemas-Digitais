48b4dc6b2ec5a3da15ca320d6da60ef1ca60af56
roacosta <digo_acosta@hotmail.com>
2016-03-31 21:33:49 -0300

    Adaptado para a Placa

--- Trabalho2.v
+++ Trabalho2.v
@@ -1,42 +1,24 @@
 
-
 // Trabalho 2 Rodrigo Almeida Costa
 
 
 module Pisca(
 	input CLOCK_50,
-	output LEDG
+	output [1:0] LEDG
 	);
 
 	reg estado =0;
 	reg [32:0]cont = 0;
 
-   	assign LEDG = estado;
+   	assign LEDG[0] = estado;
 
 	always @(posedge CLOCK_50) begin
-		if (cont == 5) begin		
-	      	estado = ~estado;
-       	    cont = 0;
+		if (cont == 50000000) begin		
+	      		estado = ~estado;
+       	    		cont = 0;
 	   	end 
 	   	else begin
-        	cont = cont + 1;
+        		cont <= cont + 1;
 	   	end
 	end
-endmodule
-
-module test;
-    reg CLOCK_50;
-	wire LEDG;
-
-    	Pisca A(CLOCK_50,LEDG);
-
-    	always #1 CLOCK_50 <= ~CLOCK_50;
-
-    	initial begin
-        $dumpvars(0 , A);
-		#0;	
-        CLOCK_50 <= 0;
-		#500;
-        $finish;
-    	end
 endmodule
