fc276a5dec25bb40abf1098f13c384ce4abb1d0a
roacosta <digo_acosta@hotmail.com>
2016-04-07 01:05:59 -0300

    Alterado para gerar arquivo em Hexadecimal

--- Trabalho3.c
+++ Trabalho3.c
@@ -45,24 +45,42 @@ int main(){
 	struct fPoint p1,p2,p3,pt;
 	int InTriangle;
 	int i, j,x,y;
-	int cont = 0;2;
+	int cont = 0;
 
-	FILE *ArquivoCoordenadas;
+	FILE *ArquivoTriangulos;
 	FILE *ArquivoGerado;
 
-	ArquivoCoordenadas = fopen("ArquivoCoordenadas.txt","r");
+
 	ArquivoGerado = fopen("ArquivoGerado.txt","w");
 
 	cont = 1;
 
-	while ((fscanf(ArquivoCoordenadas,"%d %d %d %d %d %d %d %d", &p1.x ,&p1.y,&p2.x,&p2.y,&p3.x,&p3.y,&pt.y,&pt.x))!= EOF ){
+    for (i = 99; i >= 0; i--){
+        for(j = 0; j<100;j++){
+            pt.y = i; pt.x = j;
+            InTriangle = 0;
+            ArquivoTriangulos = fopen("ArquivoTriangulos.txt","r");
 
+            while ((fscanf(ArquivoTriangulos,"%d %d %d %d %d %d", &p1.x,&p1.y,&p2.x,&p2.y,&p3.x,&p3.y))!= EOF ){
+                if (InTriangle == 0){
+                    InTriangle = PointInTriangle(pt,p1,p2,p3);
+                }
+            }
+            fclose(ArquivoTriangulos);
 
-        //fscanf(ArquivoCoordenadas,"%d %d %d %d %d %d %d %d", &p1.x ,&p1.y,&p2.x,&p2.y,&p3.x,&p3.y,&pt.y,&pt.x);
-        InTriangle = PointInTriangle(pt,p1,p2,p3);
-        fprintf(ArquivoGerado,"%d %d %d %d %d %d %d %d %d \n", p1.x,p1.y,p2.x,p2.y,p3.x,p3.y,pt.x,pt.y,InTriangle);
-        //fprintf(ArquivoGerado,"%d %d %d %d %d %d %d %d\n", p1.x ,p1.y,p2.x,p2.y,p3.x,p3.y,pt.y,pt.x
-	}
+            cont++;
+            fprintf(ArquivoGerado,"%04x %d\n",cont,InTriangle);
+
+            /*if(InTriangle == 1){
+                printf("*");
+            }else{
+                printf(" ");
+            }*/
+        }
+        printf("\n");
+    }
+
+    fclose(ArquivoGerado);
 
 	//InTriangle = PointInTriangle (pt,p1,p2,p3);
 	//printf("\n%d\n\n",InTriangle);
